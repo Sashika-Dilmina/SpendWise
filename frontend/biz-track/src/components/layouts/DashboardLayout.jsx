4a972f296a9a7f53b1df40340c63157fe23a4de2
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-01-03 16:46:22 +0530

    Fix userContext import path (case-sensitive)

--- frontend/biz-track/src/components/layouts/DashboardLayout.jsx
+++ frontend/biz-track/src/components/layouts/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Navbar from './Navbar';
 
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../../context/UserContext';
 import SideMenu from './SideMenu';
 
 const DashboardLayout = ({children,activeMenu}) => {
